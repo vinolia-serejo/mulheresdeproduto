38e64a444200b1b8bd67af387f2d587d4cc425a3
Vinólia Serejo <43222539+vinolia-serejo@users.noreply.github.com>
2019-05-27 13:56:25 -0300

    projeto finalizado

--- js/main.js
+++ js/main.js
@@ -1,7 +1,5 @@
 $(document).ready(function () {
 
-  // $('img').attr('draggable', 'false');
-
   $('.m-menu').hide();
 
     
@@ -29,9 +27,49 @@ $(document).ready(function () {
       $('nav').toggleClass('nav--bg');
     }
   })
-  
-  $('.veja-mais').click(function(){
-    $('#mobile-cards').slideToggle();
+
+  $('#mobile-menu-options').find('li').click(function(){
+    $('.m-menu').slideUp('slow');
   })
 
+  // VEJA MAIS 
+  
+  $.fn.extend({
+    toggleText: function(a, b){
+        return this.text(this.text() == b ? a : b);
+    }
+  });
+
+  $('.veja-mais').click(function () {
+    $(this).toggleText('Veja Mais', 'Veja Menos');
+    $('.hide-cards').slideToggle();
+  })
+
+
 });
+
+// IDENTIFICAÇÃO DO MENU
+
+(function() {
+  'use strict';
+
+  let section = document.querySelectorAll(".section");
+  let sections = {};
+  let i = 0;
+
+  Array.prototype.forEach.call(section, function(e) {
+    sections[e.id] = e.offsetTop;
+  });
+
+  window.onscroll = function() {
+    var scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
+
+    for (i in sections) {
+      if (sections[i] <= scrollPosition) {
+        document.querySelector('.active').setAttribute('class', ' ');
+        document.querySelector('a[href*=' + i + ']').setAttribute('class', 'active');
+      }
+    }
+  };
+})();
+
